f7adf74a9772672bb1c5029f18a7a094c92f5167
NekrosTV <41417463+NekrosTV@users.noreply.github.com>
2022-05-28 18:37:26 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,4 +12,5 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 cd ~/zsh-autoconfig
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 rm ~/.zshrc; cp .zshrc ~
+rm ~/.tmux.conf; cp .tmux.conf ~
 rm ~/.p10k.zsh; cp .p10k.zsh ~
