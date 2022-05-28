d901171addcb0f2882062080c8e1e15f6a8344f1
NekrosTV <41417463+NekrosTV@users.noreply.github.com>
2022-05-28 18:37:45 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -7,4 +7,5 @@ rm -rf powerlevel10k
 cd ~/zsh-autoconfig
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 rm ~/.zshrc; cp .zshrc ~
+rm ~/.tmux.conf; cp .tmux.conf ~
 rm ~/.p10k.zsh; cp .p10k.zsh ~
